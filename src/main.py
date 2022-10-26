63ee43db2f4e0c48e3f6b1cbcf292809db8b5713
Sébastien De Vos <sebastien.de.vos@ulb.be>
2022-10-26 14:09:21 +0200

    analyzing the data on seb branch

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+import pandas
+import numpy
+import json
+
+path = "../data/gtfs3Sept/"
+file = "agency.txt"
+
+df = pandas.read_csv(path+file)
+df2 = pandas.read_json("../data/vehiclePosition01.json")
+print(df.head())
+print(df2.head())
