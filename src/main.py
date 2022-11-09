6895ddce174cbe37cecae4e539074716db736ef0
LaurentColpaert <yololoyololo2000@gmail.com>
2022-11-09 17:37:28 +0100

    cleaning the code

--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
-print("Hello World!")
+from preprocessing import load_data_and_merge,filter,compute_time_difference,retrieve_info_title
+from plotting import plot_schedulde_headways
+
+
+
+def show_schedulde_headways(direction_id, saterday, stop_id, short_name, start_date):
+    route = 'data/gtfs3Sept/routes.txt'
+    trips = 'data/gtfs3Sept/trips.txt'
+    stop_times = 'data/gtfs3Sept/stop_times.txt'
+    calendar = 'data/gtfs3Sept/calendar.txt'
+    data = load_data_and_merge(route,trips,stop_times,calendar)
+    data = filter(data, direction_id= direction_id, saterday=saterday, stop_id=stop_id,short_name= short_name,start_date= start_date)
+    x,y = compute_time_difference(data)
+    print(data)
+    trip_headsign, long_name, stop_name = retrieve_info_title(data,stop_id=stop_id)
+    plot_schedulde_headways(x,y,line=short_name,line_name=long_name,trip_headsign=trip_headsign,stop_name=stop_name)
+
+
+
+if __name__ == "__main__":
+    show_schedulde_headways(direction_id =0, saterday=1, stop_id= '5705', short_name = '3', start_date= 20210911)
+
