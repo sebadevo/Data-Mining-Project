aa97ee72612f445b3f551a8959a8bfd4d9b78197
Tarik <Tarik.Kalai@ulb.be>
2022-11-14 19:17:31 +0100

    Dropdown Improvement

--- src/dashboard/components/dropdown.py
+++ src/dashboard/components/dropdown.py
@@ -9,6 +9,10 @@ from database.load_db import load_dataframe
 from preprocessing import load_data_and_merge,filter,compute_time_difference,retrieve_info_title
 import pandas as pd
 
+from database.load_db import get_connection, close_connection
+
+
+
 
 def mode_render():
     return dcc.Dropdown(
@@ -25,7 +29,21 @@ def line_name_render(app: Dash):
                 Output('line-name', "disabled"),
                 [Input('mode','value')])
     def enable_dropdown(mode):  
-        return mode in ["Metro","Tram"]        
+        return mode not in ["Metro","Tram", "Bus"]  
+
+    @app.callback(
+                Output('line-name', 'options'), 
+                [Input('mode','value')]
+    )
+    def query_route_short_name(mode):
+        query = (
+            "select routes_short_name,  routes_long_name from routes where route_type = %s;"
+            )
+        connection = get_connection()
+        data = pd.read_sql(query, params=[mode], con= connection)
+
+        data['name'] = data.iloc[:,:2].apply(lambda x: ','.join(x.astype(str)), axis=1)
+        return data.name.tolist()
 
     return dcc.Dropdown(
                 options = ["Bus","Tram","hoho"],
@@ -35,3 +53,4 @@ def line_name_render(app: Dash):
                 className='form-dropdown drop',
                 placeholder="Select the line",
                 persistence="string")
+    
