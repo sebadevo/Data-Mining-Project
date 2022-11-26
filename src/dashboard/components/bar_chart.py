22503af9d90da61296b719f82f5a581656a475d9
sebadevo <sebastien.de.vos@ulb.be>
2022-11-26 11:15:10 +0100

    addition of the line interval on the dashboard

--- src/dashboard/components/bar_chart.py
+++ src/dashboard/components/bar_chart.py
@@ -1,7 +1,7 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 from dashboard.enumeration import Type,Day
-
+from utils import *
 from dash.dependencies import Output, Input
 from . import ids
 
@@ -18,16 +18,21 @@ def render(app: Dash) -> html.Div:
         def load_data(type = Type.Tram,day= Day.Saterday, direction_id =0, stop_id= '5705', short_name = '3', start_date= 20210911):
             data = load_dataframe(day,type.value,direction_id =direction_id, stop_id= stop_id, short_name = short_name, start_date= start_date)
             x,y = compute_time_difference(data)
+            lines = get_interval(x, y)
             data = pd.DataFrame()
             data["x"] = x
             data["y"] = y
             print(data)
-            return data
+            return data, lines
 
         if mode not in ["0","1","3"] :
             return html.Div("chey",id=ids.BAR_CHART)
-        data = load_data(type = Type.Tram,day= Day.Saterday, direction_id =0, stop_id= '5705', short_name = '3', start_date= 20210911)
+        data, lines = load_data(type = Type.Tram,day= Day.Saterday, direction_id =0, stop_id= '5705', short_name = '3', start_date= 20210911)
         fig = px.line(data,x="x",y="y")
+        for line in lines:
+            fig.add_vline(x = line, line_color = 'red')
+
+
         return html.Div(dcc.Graph(figure=fig), id=ids.BAR_CHART)
 
     return html.Div(id=ids.BAR_CHART)
