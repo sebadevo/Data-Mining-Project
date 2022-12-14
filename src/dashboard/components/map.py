a076ca7ac8d815689df8751faec2179fbec7c70a
LaurentColpaert <yololoyololo2000@gmail.com>
2022-12-14 17:46:20 +0100

    adding maps

--- src/dashboard/components/map.py
+++ src/dashboard/components/map.py
@@ -23,7 +23,19 @@ bus = ["12","13","14","T19","20","21","27","28","29","33","34","36","37","38",
     "64","65","66","69","70","71","72","73","74","75","76","77","78","79","80","T81","T82","83","86","87","88","89","90","T92","95"]
 noctis = ["04","05","06","08","09","10","11","12","13","16","18"]
 
-stib_map = folium.Map(
+
+def get_data():
+    query = (
+            "select distinct tr.direction_id"
+            " from trips tr" 
+            " inner join routes ro on tr.route_id = ro.routes_id"
+            " where ro.route_type = %s and ro.routes_short_name = %s and  tr.trip_headsign = %s" # how do I use direction in this SQL querry ????
+            )
+    connection = get_connection()
+    direction = pd.read_sql(query, params=[], con= connection)        
+        
+def create_map():
+    stib_map = folium.Map(
         location=[50.8476, 4.3572],
         zoom_start=12,
         # tiles = 'MapBox Bright'
@@ -33,92 +45,14 @@ stib_map = folium.Map(
         tiles='cartodbpositron'
     )
 
-stib_map.save('map_save.html')
+    stib_map.save('map_save.html')
 
-def display_map():
+
+def display_map(line):
     return html.Div([
         html.A('Interactive STIB Map'),
-        html.Iframe(id=ids.MAP, srcDoc=open('map_save.html', 'r').read(), width='100%', height='600')
+        html.Iframe(id=ids.MAP, srcDoc=open(f'src/map/stib_map_{line}.html', 'r').read(), width='100%', height='600')
     ])
-
-def which_type(value):
-    if value in metro:
-        return 1
-    return 0 if value in tram else 3
-
-
-
-def which_date(value): 
-    return value.split(" - ")
-
-def which_day(value): 
-    Weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-    return "Weekday" if value in Weekdays else value
-
-def render(app: Dash) -> html.Div:
-    @app.callback(
-            Output(ids.MAP, "data"),
-            State(ids.SELECTED_LINE,'data'),
-            State(ids.STOP, 'value'),
-            State(ids.DATE, 'value'),
-            Input(ids.REAL_DATE, 'value'),
-            prevent_initial_call=True
-            )
-
-    def update_map(line_name, stop_name,date, real_date_name) -> html.Div:  
-        stop_name = stop_name.split(' - ')[0]
-        real_date_name, day = real_date_name.split(' - ')
-        day = which_day(day)
-        if (which_type(line_name)==1): #For metros
-            query = ( 
-                "select rd.time"
-                " from routes ro, trips tr, stop_times st" 
-                " inner join routes ro on tr.route_id = ro.routes_id"
-                " inner join stop_times st on st.trip_id = tr.trip_id"
-                " where ro.route_short_name = %s and "
-                )
-        else : 
-            return html.Div(html.H4("This feature has not been implemented yet, would you kindly select a line from a metro and go on as if nothing happened ? \n"
-            "from the developpers team."))
-        connection = get_connection()
-        start_time = process_time()
-        real_data = pd.read_sql(query, params=[line_name, stop_name, real_date_name], con= connection)
-        query = (
-            "select st.arrival_time, c.monday, c.saturday, c.sunday"
-            " from trips tr" 
-            " inner join routes ro on tr.route_id = ro.routes_id"
-            " inner join stop_times st on st.trip_id = tr.trip_id"
-            " inner join stops s on s.stop_id = st.stop_id"
-            " inner join calendar c on c.service_id = tr.service_id"
-            " where ro.route_type = %s and ro.routes_short_name = %s and st.stop_id = %s and c.start_date = %s and c.end_date = %s"
-            )
-        connection = get_connection()
-        data = pd.read_sql(query, params=[which_type(line_name), line_name, stop_name, int(which_date(date)[0]), int(which_date(date)[1])], con= connection)
-
-        if( len(real_data) < 5):
-            return html.Div(html.H4(f"There is not enough data to plot a graph (number of lines in the data is: {len(real_data)})"))
-        
-        
-        
-        time_real = map_to_sec(real_data.time.tolist())
-        time_th = map_to_sec(data.arrival_time.tolist())
-
-
-        time_real = remove_duplicates(time_real)
-
-
-        x,y = get_headway(time_th)
-        intervals = get_interval(x,y)
-
-        if (len(time_th) < len(time_real)): 
-            scheduled_times, real_times = find_match_V2(time_th, time_real)
-        else: 
-            real_times, scheduled_times = find_match_V2(time_real, time_th)
-
-
-        real_headways_x,real_headways_y = get_headway(real_times)
-        scheduled_headways_x,scheduled_headways_y = get_headway(scheduled_times)
-        qualities = interval_score(scheduled_times, real_times, scheduled_headways_x, real_headways_x, scheduled_headways_y, real_headways_y, intervals)
-
-        return plot_interval_scores(qualities, intervals, day)
-    return html.Div(id=ids.INTERVAL_SCORE_CHART)
+    
+def display_all_map():
+    return html.Div([html.A('Interactive STIB Map'), html.Iframe(id=ids.MAP, srcDoc=open('src/map/stib_map.html', 'r').read(), width='100%', height='600')])
